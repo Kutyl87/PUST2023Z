dbc81a2ea27a2e58163db8262a1bbd577a2e4467
dfokashch <01174305@pw.edu.pl>
2023-11-08 23:50:40 +0100

    Updated step functions

--- PROJ/src/get_s_u.m
+++ PROJ/src/get_s_u.m
@@ -2,7 +2,8 @@ function s = get_s_u(Upp, Ypp, sim_end)
     % Returns vector of step response coefficients for steeling
     u_end = 1;
     y = step_simulation(u_end, 0, sim_end);
-    s = zeros(1, sim_end);
+    y = y(1,3:length(y));
+    s = zeros(1, length(y));
     for i=1:length(s)
         s(i) = (y(i) - Ypp) / (u_end - Upp);
     end
--- PROJ/src/get_s_z.m
+++ PROJ/src/get_s_z.m
@@ -2,7 +2,8 @@ function s = get_s_z(Zpp, Ypp, sim_end)
     % Returns vector of step response coefficients for interference
     z_end = 1;
     y = step_simulation(0, z_end, sim_end);
-    s = zeros(1, sim_end);
+    y = y(1,3:length(y));
+    s = zeros(1, length(y));
     for i=1:length(s)
         s(i) = (y(i) - Ypp) / (z_end - Zpp);
     end
