e94e27d928f78f774e592e5e612d4d07159da661
mchomans <01168876@pw.edu.pl>
2023-11-10 19:40:48 +0100

    add stuff

--- PROJ/src/get_s_z.m
+++ PROJ/src/get_s_z.m
@@ -2,7 +2,7 @@ function s = get_s_z(Zpp, Ypp, sim_end)
     % Returns vector of step response coefficients for interference
     z_end = 1;
     y = step_simulation(0, z_end, sim_end);
-    y = y(1,3:length(y));
+    y(3:end);
     s = zeros(1, length(y));
     for i=1:length(s)
         s(i) = (y(i) - Ypp) / (z_end - Zpp);
