13c38bf1899a0cddf30f75e81ed21158a851fe04
dfokashch <01174305@pw.edu.pl>
2023-12-02 12:44:43 +0100

    Added zadanie 2

---
+++ PROJEKT3/PROJ/SRC/zadanie2_st.m
@@ -0,0 +1,13 @@
+sim_end = 500;
+possible_u = -1:0.01:1;
+stat_y = [];
+for i= 1:size(possible_u, 2)
+    y_step = step_simulation(possible_u(i), sim_end);
+    stat_y(i) = y_step(end);
+end
+
+plot(possible_u, stat_y)
+title('Zależność wyjścia od sterowania')
+xlabel('Sterowanie')
+ylabel('Wyjście')
+matlab2tikz ('zad2_stat.tex' , 'showInfo' , false)
