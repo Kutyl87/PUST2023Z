c7c7a83b67e3befd5579491d7595f13865ab3372
mikic202 <mikolaj.chomanski@gmail.com>
2023-12-03 19:15:16 +0100

    add ploting to tex files

--- PROJEKT3/PROJ/SRC/main.m
+++ PROJEKT3/PROJ/SRC/main.m
@@ -1,12 +1,16 @@
-trapezoidal_membership_function(0.5, 3, [[-2, -1, -0.5, 0]; [-1, -0.5, 0.5, 1]; [0, 0.5, 1, 2]])
-figure
-weights = [];
-x = [-1:0.05:1];
-for i=1:41
-    weights(i, :) = trapezoidal_membership_function(x(i), 3, [[-2, -1, -0.25, -0.05]; [-0.25, -0.05, 0.2, 0.4]; [0.2, 0.4, 1, 2]]);
-end
-
-plot(x, weights)
+% trapezoidal_membership_function(0.5, 3, [[-2, -1, -0.5, 0]; [-1, -0.5, 0.5, 1]; [0, 0.5, 1, 2]])
+% figure
+% weights = [];
+% x = [-1:0.05:1];
+% for i=1:41
+%     weights(i, :) = trapezoidal_membership_function(x(i), 3, [[-2, -1, -0.25, -0.05]; [-0.25, -0.05, 0.2, 0.4]; [0.2, 0.4, 1, 2]]);
+% end
+% 
+% plot(x, weights)
+% title('Trapezoidalna funcja przynależności')
+% xlabel('Sterowanie')
+% ylabel('Waga')
+% matlab2tikz ('zad_5_trapez_3.tex' , 'showInfo' , false, 'standalone', true) 
 % 
 % 
 % figure
@@ -18,11 +22,14 @@ plot(x, weights)
 % 
 % plot(x, weights)
 
-% figure
-% weights = [];
-% x = [-1:0.05:1];
-% for i=1:41
-%     weights(i, :) = bell_membership_function(x(i), 3, [[-1.3, 1.1, 10]; [0.1, 0.2, 4]; [1.3, 0.9, 7]]);
-% end
-% 
-% plot(x, weights)
+figure
+weights = [];
+x = [-1:0.01:1];
+for i=1:size(x, 2)
+    weights(i, :) = bell_membership_function(x(i), 3, [[-1.3, 1.1, 10]; [0.1, 0.3, 3];[1.3, 0.9, 7]]);
+end
+plot(x, weights)
+title('Dzwonowa funcja przynależności')
+xlabel('Sterowanie')
+ylabel('Waga')
+matlab2tikz ('zad_5_dzwon_3.tex' , 'showInfo' , false, 'standalone', true) 
